6a910d1b77bfc995742fdff38f8faedf2e2ceb68
Jaxing <jesper_jaxing@hotmail.com>
2016-01-20 18:39:48 +0100

    Added a file to test the methods in Uppg1.java

---
+++ src/Lab1/test.java
@@ -0,0 +1,12 @@
+package Lab1;
+
+import static Lab1.Uppg1.binarySqrt;
+
+/**
+ * Created by jesper on 2016-01-20.
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.print(binarySqrt(16,0.000001));
+    }
+}
