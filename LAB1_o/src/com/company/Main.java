d1d5020e621e29b1fc5fcc0247130a1dc0fd339c
ohenriksson <o.henriksson@gmail.com>
2016-01-21 09:54:01 +0100

    LAB1_del1 klar

--- LAB1_o/src/com/company/Main.java
+++ LAB1_o/src/com/company/Main.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-
+    MySqrt.main(args);
 
 	// write your code here
     }
--- LAB1_o/src/com/company/MySqrt.java
+++ LAB1_o/src/com/company/MySqrt.java
@@ -6,8 +6,6 @@ import java.lang.Math;
  * Created by Oskar on 21-Jan-16.
  */
 public class MySqrt {
-
-
     public static double mySqrtLoop(double x, double eps) {
         double Ymin,Ymax,Ymid,YmidSq;
 
@@ -21,16 +19,15 @@ public class MySqrt {
             Ymin = 1;
             Ymax = x;
         }
-
-        //Algorithm
-        while(true) {
+        
+        while(true) {       //Algorithm
             Ymid = (Ymin + Ymax) / 2;
             YmidSq = Math.pow(Ymid, 2); //Assign Ymid Squared
             if (Math.abs(YmidSq - x) <= eps) {
                 break; //done! Ymid is good enaught
-            } else if (YmidSq > x) {
-                Ymin = Ymid; //Look in the top half
             } else if (YmidSq < x) {
+                Ymin = Ymid; //Look in the top half
+            } else{ // (YmidSq > x)
                 Ymax = Ymid; //Look in the bottom half
             }
         }
@@ -39,9 +36,17 @@ public class MySqrt {
     //public static double mySqrtRecurse(double x, double eps){}
     public static void main(String [] args){
         double eps = Math.pow(10,-6);
-        double x = 9;
+        checkValue(-1,eps);
+        checkValue(0,eps);
+        checkValue(0.5,eps);
+        checkValue(1,eps);
+        checkValue(9,eps);
 
-        System.out.println("Ymid:" +mySqrtLoop(x,eps));
+    }
+
+    private static void checkValue(double x,double eps){
+        double diff = Math.pow(mySqrtLoop(x,eps),2) - x;
+        System.out.println("x=" +x +" Ymid^2-x=" +diff +" eps:" +eps +" diff<eps:" +(diff<eps));
 
     }
 }
