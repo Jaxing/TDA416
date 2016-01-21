fe1088e44886010c621a3ceceba7c1cbac5563fa
ohenriksson <o.henriksson@gmail.com>
2016-01-21 08:22:32 +0100

    created MySqrt class

---
+++ LAB1_o/src/com/company/MySqrt.java
@@ -0,0 +1,13 @@
+package com.company;
+
+/**
+ * Created by Oskar on 21-Jan-16.
+ */
+public class MySqrt {
+    public static double mySqrtLoop(double x, double eps) {
+        
+
+    }
+    //public static double mySqrtRecurse(double x, double eps){}
+    public static void main(String [] args){}
+}
