79d67f97c579acab9c83c8aab3dff66f06616ab6
ohenriksson <o.henriksson@gmail.com>
2016-01-21 09:11:42 +0100

    LAB1_del1 Nästan klar

--- LAB1_o/src/com/company/MySqrt.java
+++ LAB1_o/src/com/company/MySqrt.java
@@ -9,10 +9,18 @@ public class MySqrt {
 
 
     public static double mySqrtLoop(double x, double eps) {
-        double Ymin = 0;
-        double Ymax = 0;
-        double Ymid = 0;
-        double YmidSq = 0; //Ymid Squared
+        double Ymin,Ymax,Ymid,YmidSq;
+
+        //Set initial interval based on the value of x.
+        if(x<0){ //no real root exists
+            return Double.NaN;
+        } else if(0 <= x && x <= 1){ //root must be within [x,1]
+            Ymin = x;
+            Ymax = 1;
+        } else{ //(x>1) root must be within [1,x]
+            Ymin = 1;
+            Ymax = x;
+        }
 
         //Algorithm
         while(true) {
@@ -20,18 +28,20 @@ public class MySqrt {
             YmidSq = Math.pow(Ymid, 2); //Assign Ymid Squared
             if (Math.abs(YmidSq - x) <= eps) {
                 break; //done! Ymid is good enaught
-            } else if (YmidSq - x < 0) {
+            } else if (YmidSq > x) {
                 Ymin = Ymid; //Look in the top half
-            } else if (YmidSq - x >= 0) {
+            } else if (YmidSq < x) {
                 Ymax = Ymid; //Look in the bottom half
-
             }
         }
-        return 0;
+        return Ymid;
     }
     //public static double mySqrtRecurse(double x, double eps){}
     public static void main(String [] args){
         double eps = Math.pow(10,-6);
+        double x = 9;
+
+        System.out.println("Ymid:" +mySqrtLoop(x,eps));
 
     }
 }
