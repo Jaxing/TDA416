97d914b0314eb5c016cc7c04250414ebe27c7fed
ohenriksson <o.henriksson@gmail.com>
2016-02-17 09:47:57 +0100

    LAB 3 påbörjad

---
+++ LAB3_o/src/SplayTreeSet.java
@@ -0,0 +1,24 @@
+/**
+ * Created by Oskar on 15-Feb-16.
+ */
+public class SplayTreeSet<T> implements SimpleSet<Integer> {
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean add(Integer x) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Integer x) {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Integer x) {
+        return false;
+    }
+}
